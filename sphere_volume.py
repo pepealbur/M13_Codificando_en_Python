72cb73940ba0349c7e1338bfb5fa2f16a6c921ed
pepe albur <gaggs_mx@yahoo.com>
2024-08-06 13:06:05 -0600

    inital commit

---
+++ sphere_volume.py
@@ -0,0 +1,18 @@
+# *********************
+# VOLUMEN DE UNA ESFERA
+# *********************
+
+
+def run(radio: float) -> float:
+    import math
+
+
+        volumen = (4 / 3) * math.pi * radio ** 3
+        return volumen
+    volumen = 'output'pyche
+
+    return volumen
+
+
+if __name__ == '__main__':
+    run(5)
